e8897f241357280a8c0d31a69333b580a9ba6fec
Pasindu Heshan <p.heshan622@gmail.com>
2025-05-14 13:52:57 +0530

    finalize login page and create mainframe

--- src/inf/login.java
+++ src/inf/login.java
@@ -4,6 +4,8 @@
  */
 package inf;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Heshan
@@ -26,21 +28,96 @@ public class login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        unamebox = new javax.swing.JTextField();
+        pwordbox = new javax.swing.JPasswordField();
+        loginbtn = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        jPanel1.setBackground(new java.awt.Color(0, 102, 153));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        pack();
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
+        jLabel1.setText("Login Form");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 40, -1, -1));
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel2.setText("Username");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 160, -1, -1));
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel3.setText("Password");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 210, -1, -1));
+
+        unamebox.setBackground(new java.awt.Color(102, 102, 102));
+        unamebox.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        unamebox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                unameboxActionPerformed(evt);
+            }
+        });
+        jPanel1.add(unamebox, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 160, 240, 30));
+
+        pwordbox.setBackground(new java.awt.Color(102, 102, 102));
+        pwordbox.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        pwordbox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                pwordboxActionPerformed(evt);
+            }
+        });
+        jPanel1.add(pwordbox, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 210, 240, 30));
+
+        loginbtn.setBackground(new java.awt.Color(204, 204, 204));
+        loginbtn.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        loginbtn.setForeground(new java.awt.Color(0, 0, 0));
+        loginbtn.setText("Login");
+        loginbtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                loginbtnActionPerformed(evt);
+            }
+        });
+        jPanel1.add(loginbtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 290, 90, 30));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 630, 400));
+
+        setSize(new java.awt.Dimension(638, 409));
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void pwordboxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pwordboxActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_pwordboxActionPerformed
+
+    private void unameboxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_unameboxActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_unameboxActionPerformed
+
+    private void loginbtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loginbtnActionPerformed
+       
+        String uname; //Heshan
+        String pword; //1234
+        
+        uname = unamebox.getText();
+        pword = pwordbox.getText();
+        
+        if(uname.equals("Heshan") && pword.equals("1234")){
+            
+            mainframe m1 = new mainframe();
+            m1.setVisible(true);
+            this.dispose();
+        }
+        else{
+            
+            JOptionPane.showMessageDialog(null,"username or password incorrect...!");
+            
+        }
+
+    }//GEN-LAST:event_loginbtnActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +155,12 @@ public class login extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton loginbtn;
+    private javax.swing.JPasswordField pwordbox;
+    private javax.swing.JTextField unamebox;
     // End of variables declaration//GEN-END:variables
 }
